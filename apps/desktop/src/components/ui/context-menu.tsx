a059b9c589afab7c356a43b5e218425e25de271f
metal-face <hughesbryan3000@gmail.com>
2023-11-23 23:09:44 -0500

    Refactor context menu to use onEventShowMenu

--- apps/desktop/src/components/ui/context-menu.tsx
+++ apps/desktop/src/components/ui/context-menu.tsx
@@ -1,29 +1,29 @@
-import { showMenu } from "tauri-plugin-context-menu";
+import { onEventShowMenu } from "tauri-plugin-context-menu";
 import { invoke } from "@tauri-apps/api/tauri";
 import { useState } from "react";
 
 const [ctEnabled, setCtEnabled] = useState(false);
-invoke("get_clickthrough").then((clickthrough) => {
-  setCtEnabled(clickthrough as boolean);
-});
 
-showMenu({
-  items: [
-    {
-      label: "Enable Clickthrough",
-      event: () => {
-        setCtEnabled(true);
-        invoke("toggle_clickthrough");
+onEventShowMenu("contextmenu", async (_e?: MouseEvent) => {
+  const options = {
+    items: [
+      {
+        label: "Enable Clickthrough",
+        event: () => {
+          setCtEnabled(true);
+          invoke("toggle_clickthrough");
+        },
+        disabled: ctEnabled ? false : true,
       },
-      disabled: ctEnabled ? false : true,
-    },
-    {
-      label: "Disable Clickthrough",
-      event: () => {
-        setCtEnabled(false);
-        invoke("toggle_clickthrough");
+      {
+        label: "Disable Clickthrough",
+        event: () => {
+          setCtEnabled(false);
+          invoke("toggle_clickthrough");
+        },
+        disabled: ctEnabled ? true : false,
       },
-      disabled: ctEnabled ? true : false,
-    },
-  ],
+    ],
+  };
+  return options;
 });
