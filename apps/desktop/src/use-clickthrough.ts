bb5edb03b0e9c711bd9ce5cc8371913538e24605
metal-face <hughesbryan3000@gmail.com>
2023-11-24 19:06:24 -0500

    custom context menu with enable clickthrough added

--- apps/desktop/src/use-clickthrough.ts
+++ apps/desktop/src/use-clickthrough.ts
@@ -1,11 +1,36 @@
 import { useEffect, useState } from "react";
 import { listen } from "@tauri-apps/api/event";
-import overlayedConfig from "./config";
 import { invoke } from "@tauri-apps/api";
+import { showMenu } from "tauri-plugin-context-menu";
+import overlayedConfig from "./config";
 
 export const useClickthrough = () => {
   const [clickthrough, setClickthrough] = useState(false);
+
   useEffect(() => {
+    /**
+     * Listen for the native 'contextmenu' event and prevent default behavior.
+     * Then use tauri plugin to show custom context menu with option to enable
+     * clickthrough.
+     */
+    document.addEventListener("contextmenu", e => {
+      e.preventDefault();
+      showMenu({
+        items: [
+          {
+            label: "Enable Clickthrough",
+            event: () => {
+              invoke<boolean>("set_clickthrough", { value: true }).catch(e => {
+                console.error("An error has occurred", e);
+              });
+            },
+          },
+        ],
+      }).catch(e => {
+        console.error("An error occurred showing the menu", e);
+      });
+    });
+
     // sub if it changes from outside of tauri
     const unlisten = listen<boolean>("toggle_clickthrough", event => {
       setClickthrough(event.payload);
